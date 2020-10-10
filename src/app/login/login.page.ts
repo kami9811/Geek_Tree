7ecd335ab557429cdbca53e4ec3241d44671b223
kami9811 <kawakamiyuudai@MacBook-Pro-4.local>
2020-10-11 05:20:17 +0900

    OAuth completed

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,7 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { NativeStorage } from '@ionic-native/native-storage/ngx';
 import { GlobalService } from '../global.service';
 import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
+import { Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-login',
@@ -17,20 +19,31 @@ export class LoginPage implements OnInit {
 
   gitFlag: Boolean = false;
 
+  interval: any;
+
+  loginFlag: Boolean = false;
+
   constructor(
     private nativeStorage: NativeStorage,
     public gs: GlobalService,
     private iab: InAppBrowser,
+    private router: Router,
+    private alertController: AlertController,
   ) { }
 
   ngOnInit() {
+  }
+  ngOnDestroy() {
+    clearInterval(this.interval);
   }
 
   insertNative = () => {
     this.nativeStorage.setItem('login', {
       user_id: this.id,
       password: this.password,
+      login: false,
     });
+    this.intervalFuntion();
     this.postObj['user_id'] = this.id;
     const body = this.postObj;
     this.gs.http('https://kn46itblog.com/hackathon/SPAJAM2020/php_apis/checkDatabase.php', body).subscribe(
@@ -58,6 +71,57 @@ export class LoginPage implements OnInit {
   getOauth = () => {
     const browser = this.iab.create('https://github.com/login/oauth/authorize?client_id=db79d4b963293b84a753&scope=user%20public_repo', '_system');
     browser.show();
+    this.intervalFuntion();
+  }
+
+  intervalFuntion = () => {
+    this.interval = setInterval(() => {
+      this.login();
+    }, 2000);
+  }
+
+  login = () => {
+    this.postObj['user_id'] = this.id;
+    this.postObj['password'] = this.password;
+    const body = this.postObj;
+    this.nativeStorage.getItem('login').then(
+      data => {
+        this.loginFlag = data['login'];
+      }
+    );
+    if(this.loginFlag == false){
+      this.gs.http('https://kn46itblog.com/hackathon/SPAJAM2020/php_apis/checkDatabase.php', body).subscribe(
+        res => {
+          if(res['verification'] == 1){
+            console.log('login process');
+            this.gs.http('https://kn46itblog.com/hackathon/SPAJAM2020/php_apis/login.php', body).subscribe(
+              res => {
+                clearInterval(this.interval);
+                if(res['status'] == 200){
+                  console.log('login has successed');
+                  // this.alertLogin();
+                  this.nativeStorage.setItem('login', {
+                    user_id: this.id,
+                    password: this.password,
+                    login: true,
+                  });
+                  this.router.navigate(['']);
+                }
+              }
+            )
+          }
+        }
+      );
+    }
+  }
+
+  async alertLogin() {
+    const alert = await this.alertController.create({
+      message: 'ログインに成功しました.',
+      buttons: ['OK']
+    })
+
+    await alert.present();
   }
 
 }
