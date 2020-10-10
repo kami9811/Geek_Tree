738a4b9d5cefb2cf4876bbe3dca00920613d70f5
kami9811 <kawakamiyuudai@MacBook-Pro-4.local>
2020-10-11 03:11:57 +0900

    OAuth-1

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NativeStorage } from '@ionic-native/native-storage/ngx';
+import { GlobalService } from '../global.service';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +8,44 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
+  id: string;
+  password: string;
 
-  constructor() { }
+  postObj: any = {};
+  returnObj: any = {};
+
+  constructor(
+    private nativeStorage: NativeStorage,
+    public gs: GlobalService,
+  ) { }
 
   ngOnInit() {
+  }
+
+  insertNative = () => {
+    this.nativeStorage.setItem('login', {
+      user_id: this.id,
+      password: this.password,
+    });
+    this.postObj['user_id'] = this.id;
+    const body = this.postObj;
+    this.gs.http('https://kn46itblog.com/hackathon/SPAJAM2020/php_apis/checkDatabase.php', body).subscribe(
+      res => {
+        console.log(res);
+        if(res['verification'] != 1){
+          this.postObj['password'] = this.password;
+          const body = this.postObj;
+          this.gs.http('https://kn46itblog.com/hackathon/SPAJAM2020/php_apis/registerInfo.php', body).subscribe(
+            res => {
+              console.log(res);
+              if(res['status'] == 200){
+                console.log('status is 200');
+              }
+            }
+          )
+        }
+      }
+    )
   }
 
 }
